d793a33e878fa818dd1bee76a7728eb824e4887f
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2017-11-13 22:13:41 +0300

    Initial commit

---
+++ Prime.cs
@@ -0,0 +1,29 @@
+﻿namespace PrimeNumber
+{
+    public class Prime
+    {
+        public bool IsPrime(int numberToCheck)
+        {
+            #region Factors local function
+            int FactorsOfNumber()
+            {
+                var counter = 0;
+                for (int i = 1; i <= numberToCheck; i++)
+                {
+                    if (numberToCheck % i == 0)
+                    {
+                        counter++;
+                        if (counter > 2)
+                        {
+                            break;
+                        }
+                    }
+                }
+                return counter;
+            }
+            #endregion
+
+            return FactorsOfNumber() == 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prime = new Prime();
+            var result = prime.IsPrime(5) ? "Prime" : "Not Prime";
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
